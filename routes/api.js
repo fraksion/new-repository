59ee793e33235073abb18caba8f0bd78c691c2dd
fraksion <cap.kryglui@gmail.com>
2019-05-21 17:56:06 +0300

    test27

--- routes/api.js
+++ routes/api.js
@@ -176,7 +176,7 @@ function ParceResponceData(data){
   alert(testData);
 }
 */
-router.get('/test', getConfigString);
+//router.get('/test', getConfigString);
 router.get('/documents', getDocuments);
 router.get('/elements', getElementList);
 router.get('/stl', getStl);
