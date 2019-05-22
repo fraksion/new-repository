8b166e0f7253b5e9afed80668cae9e42239f2087
fraksion <cap.kryglui@gmail.com>
2019-05-22 23:40:34 +0300

    getting queryParameter test25

--- routes/api.js
+++ routes/api.js
@@ -201,9 +201,6 @@ var encodeConfigString = function(req, res) {
   };
 
   var updateConfigString = function(req, res) {
-
-    console.log("data = " + req.data);
-    console.log("simple" + req);
     console.log("body " + req.body);
     //console.log( apiUrl + '/api/elements/d/0d86c205100fae7001a39ea8/e/a7d49a58add345ddb7362051/' + req.body);
     /*request.post({
