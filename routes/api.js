eb2c3029dd8ddd3c97f775582b92f5ab0960ed52
fraksion <cap.kryglui@gmail.com>
2019-05-22 22:31:17 +0300

    getting queryParameter test9

--- routes/api.js
+++ routes/api.js
@@ -191,9 +191,9 @@ var encodeConfigString = function(req, res) {
     res.send(data);
   }),
     function (error, response, body) {
-        if (!error && response.statusCode == 200) {
+
             console.log("body=" + body);
-        }
+        
     }
   };
 
