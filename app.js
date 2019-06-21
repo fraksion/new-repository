8e072231fd873a85e8681dd9865e4db6c86dd2ff
fraksion <cap.kryglui@gmail.com>
2019-06-21 09:29:55 +0300

    test 3

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ app.use('/oauthRedirect',
           // reply is null when the key is missing
           if (reply != null) {
               var newParams = JSON.parse(reply);
-              var url = '/';//?' + 'documentId=' + '0d86c205100fae7001a39ea8' + '&workspaceId=' + 'aae7a1ff196df52c5a4c153c' + '&elementId=' + 'a7d49a58add345ddb7362051';
+              var url = '';//?' + 'documentId=' + '0d86c205100fae7001a39ea8' + '&workspaceId=' + 'aae7a1ff196df52c5a4c153c' + '&elementId=' + 'a7d49a58add345ddb7362051';
               res.redirect(url);
           }
       });
