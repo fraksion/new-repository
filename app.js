b6de45a5dd7c66653a03632f718b9077aecb03a4
fraksion <cap.kryglui@gmail.com>
2019-06-24 15:10:08 +0300

    test

--- app.js
+++ app.js
@@ -65,8 +65,11 @@ app.use(passport.session());
 
 app.use('/api', api);
 app.get('/',  (req,res)=>{
-  if (process.env.test === undefined)
-    index.renderPage(req,res);
+  index.renderPage(req,res);
+  if (process.env.test === undefined){
+    window.location.pathname  = '/oauthSignin';
+  }
+    
   });
 app.get('/grantDenied', grantDenied.renderPage);
 
